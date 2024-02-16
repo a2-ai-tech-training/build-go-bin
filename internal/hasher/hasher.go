7dfd9e7d58c6832f0c00ac40500f02413a0b9e67
Kelley Ballentine <alt4kcb@gmail.com>
2024-02-16 17:43:10 -0500

    cleanup, prints from each

--- internal/hasher/hasher.go
+++ internal/hasher/hasher.go
@@ -3,6 +3,7 @@ package hasher
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 )
 
 /*
@@ -21,6 +22,6 @@ func Hasher(args []string) string {
 	hash := hex.EncodeToString(hasher.Sum(nil))
 
 	//hash_path := fmt.Sprintf("outputs/%s.txt", hash)
-	//fmt.Println(hash_path)
+	fmt.Printf("Hash for %s: %s", args, hash)
 	return hash
 }
