53a5450e76914e422f6e86105e8689f83b72c2ac
Kelley Ballentine <alt4kcb@gmail.com>
2024-02-16 17:36:52 -0500

    formatting

--- internal/hasher/hasher_test.go
+++ internal/hasher/hasher_test.go
@@ -2,6 +2,8 @@ package hasher
 
 import (
 	//"fmt"
+	"fmt"
+
 	"github.com/stretchr/testify/assert"
 	//"os"
 	"testing"
@@ -42,11 +44,17 @@ func TestHasher(t *testing.T) {
 		}
 	*/
 	test1 := []string{""}
+	e_1 := "d41d8cd98f00b204e9800998ecf8427e"
 	test2 := []string{"--json"}
+	e_2 := "31d5e31c2e1e0b3c281f5194f130287e"
 	test3 := []string{"-vvv"}
+	e_3 := "0cc598961dc9ae41055f83d0950544f3"
 
-	assert.Equal(t, Hasher(test1), "d41d8cd98f00b204e9800998ecf8427e")
+	assert.Equal(t, Hasher(test1), e_1)
+	fmt.Printf('Expected Hash for "":%s\n', e_1)
 	assert.Equal(t, Hasher(test2), "31d5e31c2e1e0b3c281f5194f130287e")
+	fmt.Printf('Expected Hash for "--json":%s\n', e_2)
 	assert.Equal(t, Hasher(test3), "0cc598961dc9ae41055f83d0950544f3")
+	fmt.Printf('Expected Hash for "-vvv":%s\n', e_3)
 
 }
