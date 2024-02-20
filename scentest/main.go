4a10e32a409202a04a52570f130cf9f74bf3df9b
Kelley Ballentine <alt4kcb@gmail.com>
2024-02-20 16:00:16 -0500

    struct is pwd, nothing works

--- scentest/main.go
+++ scentest/main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 	"log"
@@ -7,29 +8,27 @@ import (
 	"os/exec"
 )
 
-//func get-scenario-output()
+func getScenarioOutput(path string) (ScenarioOutput, error) {
+	cd ../
+}
 
 func main() {
 	//cmd := "/bin/ls /"
-	//scenario := os.Getenv("SCENARIO")
-
+	scenario := os.Getenv("SCENARIO")
 	//args := ["/bin/ls", "/"]
 	cmd2 := exec.Command("../bin/sinfo")
 	err := cmd2.Run()
-	fmt.Printf("$SCENARIO: %s\n", os.Getenv("SCENARIO"))
-	fmt.Printf("$Stdin    : %s\n", os.Stdin)
-
 	if err != nil {
 		log.Fatalf("Error running command: %v", err)
 	}
 
 	//fmt.Println(os.Environ())
-	fmt.Printf("Stdin:\n%s\n", cmd2.Args)
-	//fmt.Println(scenario)
+	fmt.Printf("Standard Input:\n%s\n", cmd2.Args)
+	fmt.Println(scenario)
 	//fmt.Println(cmd2.Stdin)
-	fmt.Printf("Standard Output:\n%s\n", cmd2.Stdout)
-	fmt.Printf("Standard Error:\n%s\n", cmd2.Stderr)
+	fmt.Printf("Standard Output:\n%s\n", os.Stdout)
+	fmt.Printf("Standard Error:\n%s\n", os.Stderr)
 	fmt.Printf("Exit Code:\n%d\n", cmd2.ProcessState.ExitCode())
-	//fmt.Printf(os.Exit(exitCode))
 	//fmt.Println(cmd2.ProcessState.ExitCode())
 }
+*/
