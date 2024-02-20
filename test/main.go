ea6cb197c1eb4fd0f1db42f0dcb6f4f90d71ab7e
Kelley Ballentine <alt4kcb@gmail.com>
2024-02-20 01:28:18 -0500

    WIP

--- test/main.go
+++ test/main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
-	//"os"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	//cmd := "/bin/ls /"
-	args := ["/bin/ls", "~"]
-	cmd2 := exec.Command(args)
+	scenario := os.Getenv("SCENARIO")
+	//args := ["/bin/ls", "/"]
+	cmd2 := exec.Command("../bin/sinfo")
 	_, err := cmd2.CombinedOutput()
 	if err != nil {
 		log.Fatalf("Error running command: %v", err)
 	}
-
 	//fmt.Println(os.Environ())
 	fmt.Printf("Standard Input:\n%s\n", cmd2.Args)
+	fmt.Println(scenario)
 	//fmt.Println(cmd2.Stdin)
 	fmt.Printf("Standard Output:\n%s\n", cmd2.Stdout)
 	fmt.Printf("Standard Error:\n%s\n", cmd2.Stderr)
