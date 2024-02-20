8a7b74ba7f8f39f72a47669777d5c6669ff80609
Kelley Ballentine <alt4kcb@gmail.com>
2024-02-20 13:37:46 -0500

    scaffolding

--- test/main.go
+++ test/main.go
@@ -7,21 +7,24 @@ import (
 	"os/exec"
 )
 
+//func get-scenario-output()
+
 func main() {
 	//cmd := "/bin/ls /"
 	scenario := os.Getenv("SCENARIO")
 	//args := ["/bin/ls", "/"]
 	cmd2 := exec.Command("../bin/sinfo")
-	_, err := cmd2.CombinedOutput()
+	err := cmd2.Run()
 	if err != nil {
 		log.Fatalf("Error running command: %v", err)
 	}
+
 	//fmt.Println(os.Environ())
 	fmt.Printf("Standard Input:\n%s\n", cmd2.Args)
 	fmt.Println(scenario)
 	//fmt.Println(cmd2.Stdin)
-	fmt.Printf("Standard Output:\n%s\n", cmd2.Stdout)
-	fmt.Printf("Standard Error:\n%s\n", cmd2.Stderr)
+	fmt.Printf("Standard Output:\n%s\n", os.Stdout)
+	fmt.Printf("Standard Error:\n%s\n", os.Stderr)
 	fmt.Printf("Exit Code:\n%d\n", cmd2.ProcessState.ExitCode())
 	//fmt.Println(cmd2.ProcessState.ExitCode())
 }
